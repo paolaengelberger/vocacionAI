1bfe1ac5755990e7224e5c933684ac6cb888d219
paolaengelberger <paoe2006@gmail.com>
2023-05-23 23:50:43 -0400

    remove bandera emoji

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import openai
 
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
-st.title("Vocación AI 🇨🇱")
+st.title("Vocación AI ")
 st.markdown("Este es como una prueba de concepto de cómo usar inteligencia artificial para sugerir carreras a estudiantes")
 st.markdown("Este es un proyecto creado por Emilia, Daniela y Paola")
 # Define the questions and answers
